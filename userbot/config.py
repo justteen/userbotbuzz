7759280f407a611bf60d1922f6cea77b237b71db
justteen <48372964+justteen@users.noreply.github.com>
2021-07-16 03:28:09 +0700

    Create config.py

---
+++ userbot/config.py
@@ -0,0 +1,18 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cb86b29472976d0d0a8a4.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ALIVE_NAME = getenv("ALIVE_NAME")
